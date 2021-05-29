4956895b4c5ec5c9a1603461b9a2063668e102c9
Adrian Macneil <adrian@foxglove.dev>
2021-05-28 17:11:09 -0700

    Set jsx:true for react

--- react.js
+++ react.js
@@ -4,6 +4,9 @@ module.exports = {
   },
   plugins: ["react", "react-hooks"],
   extends: ["plugin:react/recommended"],
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+  },
   settings: {
     react: {
       version: "detect",
