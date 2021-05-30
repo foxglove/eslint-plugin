24a058c0af229755d8e4d1858659c8980b90040a
Adrian Macneil <adrian@foxglove.dev>
2021-05-30 11:10:58 -0700

    Always extend base configuration

--- react.js
+++ react.js
@@ -1,5 +1,9 @@
 module.exports = {
-  extends: ["plugin:react/recommended", "plugin:react-hooks/recommended"],
+  extends: [
+    "./index.js",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
   env: {
     browser: true,
   },
