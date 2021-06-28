6ccb52b5bfaab09d52cfb2ca240ee0b326547f5f
Adrian Macneil <adrian@foxglove.dev>
2021-06-27 21:16:36 -0700

    Add jest config to default export

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   configs: {
     base: require("./configs/base"),
+    jest: require("./configs/jest"),
     react: require("./configs/react"),
     typescript: require("./configs/typescript"),
   },
