4302354e628ec94e20233dada0769902d53dd7a0
Jacob Bandes-Storch <jacob@bandes-stor.ch>
2021-07-06 22:51:18 -0700

    Add `no-return-promise-resolve` rule

--- configs/typescript.test.ts
+++ configs/typescript.test.ts
@@ -19,5 +19,46 @@ void (async () => {
   wut == null ? 0 : 1;
 };
 
+// wrap in an async function to ensure no false positive in nested non-async functions
+async function noReturnPromiseResolve() {
+  await Promise.resolve();
+  function foo() {
+    return Promise.resolve();
+    return Promise.resolve<number>(42);
+    return Promise.reject(42);
+    return Promise.reject<number>(42);
+  }
+  void foo;
+  void (function () {
+    return Promise.resolve();
+  })();
+
+  async function fooAsync() {
+    return Promise.resolve(); // eslint-disable-line @foxglove/no-return-promise-resolve
+    return Promise.resolve<number>(42); // eslint-disable-line @foxglove/no-return-promise-resolve
+    return Promise.reject(42); // eslint-disable-line @foxglove/no-return-promise-resolve
+    return Promise.reject<number>(42); // eslint-disable-line @foxglove/no-return-promise-resolve
+  }
+  void fooAsync;
+  void (async function () {
+    return Promise.resolve(); // eslint-disable-line @foxglove/no-return-promise-resolve
+  })();
+
+  () => Promise.resolve(42);
+  async () => Promise.resolve(42); // eslint-disable-line @foxglove/no-return-promise-resolve
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async () => {
+    Promise.resolve(42); // eslint-disable-line @typescript-eslint/no-floating-promises
+  };
+
+  () => Promise.reject(42);
+  async () => Promise.reject(42); // eslint-disable-line @foxglove/no-return-promise-resolve
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async () => {
+    Promise.reject(42); // eslint-disable-line @typescript-eslint/no-floating-promises
+  };
+}
+void noReturnPromiseResolve;
+
 // keep isolatedModules happy
 export default {};
