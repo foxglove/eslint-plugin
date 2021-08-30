7b6f77ada953fb5370fefd93bae3449ccf27a361
Jacob Bandes-Storch <jacob@bandes-stor.ch>
2021-08-30 12:04:16 -0700

    remove commented code

--- rules/strict-equality.test.ts
+++ rules/strict-equality.test.ts
@@ -23,22 +23,7 @@ ruleTester.run("strict-equality", rule, {
     "x === 5",
     "x !== 5",
   ],
-  /*
-    x === null; // eslint-disable-line @foxglove/strict-equality
-  x !== null; // eslint-disable-line @foxglove/strict-equality
-  x == null;
-  x != null;
-  x === undefined; // eslint-disable-line @foxglove/strict-equality
-  x !== undefined; // eslint-disable-line @foxglove/strict-equality
-  x == undefined;
-  x != undefined;
 
-  // require triple equals elsewhere
-  x === 5;
-  x !== 5;
-  x == 5; // eslint-disable-line @foxglove/strict-equality
-  x != 5; // eslint-disable-line @foxglove/strict-equality
-  */
   invalid: [
     {
       code: "x === null",
