447f2a0f6846bb4396c15c51f8353662fc33a583
Jacob Bandes-Storch <jacob@bandes-stor.ch>
2021-09-08 23:49:33 -0700

    Use upstream version of `no-meaningless-void-operator`

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ module.exports = {
     "license-header": require("./rules/license-header"),
     "no-private-identifier": require("./rules/no-private-identifier"),
     "strict-equality": require("./rules/strict-equality"),
-    "no-meaningless-void-operator": require("./rules/no-meaningless-void-operator"),
     "no-return-promise-resolve": require("./rules/no-return-promise-resolve"),
     "no-boolean-parameters": require("./rules/no-boolean-parameters"),
   },
