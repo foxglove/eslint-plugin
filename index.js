691d354e6db153074f7f6206d7d4141628f303de
Jacob Bandes-Storch <jacob@foxglove.dev>
2023-05-01 11:43:09 -0700

    Add prefer-hash-private rule

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ module.exports = {
     "no-return-promise-resolve": require("./rules/no-return-promise-resolve"),
     "no-boolean-parameters": require("./rules/no-boolean-parameters"),
     "no-restricted-imports": require("./rules/no-restricted-imports"),
+    "prefer-hash-private": require("./rules/prefer-hash-private"),
   },
   configs: {
     base: require("./configs/base"),
