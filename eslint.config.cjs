7ac0a85a0fe805a80017069772548eb703d36973
Adrian Macneil <adrian@foxglove.dev>
2026-01-06 12:19:19 -0800

    versions

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,33 +1,21 @@
 const globals = require("globals");
 const tseslint = require("typescript-eslint");
 
-const foxglove = require(".");
+const foxglove = require("./index");
 
 const { base, typescript, react, jest } = foxglove.configs;
 
 module.exports = tseslint.config(
-  {
-    ignores: ["dist"],
-  },
+  { ignores: ["dist"] },
   ...base,
   {
     languageOptions: {
-      globals: {
-        ...globals.node,
-      },
-      parserOptions: {
-        project: "tsconfig.json",
-      },
+      globals: { ...globals.node },
+      parserOptions: { project: "tsconfig.json" },
     },
   },
-  ...typescript.map((config) => ({
-    ...config,
-    files: ["**/*.@(ts|tsx)"],
-  })),
-  ...react.map((config) => ({
-    ...config,
-    files: ["**/*.@(jsx|tsx)"],
-  })),
+  ...typescript.map((config) => ({ ...config, files: ["**/*.@(ts|tsx)"] })),
+  ...react.map((config) => ({ ...config, files: ["**/*.@(jsx|tsx)"] })),
   ...jest.map((config) => ({
     ...config,
     files: ["**/*.test.@(js|jsx|ts|tsx)"],
